a4aa69ea0003ea0c72e6ff9aecc5526910a3c1bd
MosheHadadG <Moshehadad500@gmail.com>
2022-07-16 18:27:18 +0300

    Change Url

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     const userLoggedIn = await getMyUser(token);
     if(!userLoggedIn) return;
     setUserLoggedIn(userLoggedIn.user);
-    setSocket(io("ws://travel-in-southamerica-api.herokuapp.com"));
+    setSocket(io("ws://localhost:5050"));
     user.user = userLoggedIn.user;
     localStorage.setItem('user', JSON.stringify(user));
   } 
